9c6dca81131e0e77ca6bd4e261ec7556b26bdd9b
Veres <avrx@c02dv7c6md6m.mynet>
2021-03-19 14:09:21 +0000

    labels for segments and categories

--- src/RadialChart2.tsx
+++ src/RadialChart2.tsx
@@ -17,8 +17,22 @@ const RadialChart2: FC<BarChartProps> = ({ data }) => {
     .domain([0, 8, 16, 24])
     .range(['#D91448', '#592556', '#38379F']);
   //const createArc = d3.arc().padAngle(0.05).cornerRadius(3);
+
+  const xScale = d3
+    .scaleLinear()
+    .domain([1, 24])
+    .range([0, 2 * Math.PI]);
+
+  const xScaleSegments = d3
+    .scaleLinear()
+    .domain([1, 3])
+    .range([0, 2 * Math.PI]);
   return (
-    <svg width={width} height={height} style={{ overflow: 'visible' }}>
+    <svg
+      width={width}
+      height={height}
+      style={{ overflow: 'visible', margin: 100 }}
+    >
       {/* <g transform={`translate(${height / 2} ${width / 2})`}>
         {d3.range(180).map((d: any, i: number) => {
           return (
@@ -48,17 +62,6 @@ const RadialChart2: FC<BarChartProps> = ({ data }) => {
           strokeDasharray={3}
           opacity={0.4}
         />
-
-        {[1, 2, 3].map((idx: number) => (
-          <line
-            transform={`rotate(${120 * idx - 37})`}
-            x1={0}
-            y1={0}
-            y2={height / 2}
-            stroke={'white'}
-            strokeWidth={3}
-          />
-        ))}
         {new Array(24).fill(0).map((d: any, idx: number) => (
           <line
             transform={`rotate(${(360 / 24) * idx + 90})`}
@@ -69,10 +72,67 @@ const RadialChart2: FC<BarChartProps> = ({ data }) => {
             strokeWidth={1}
           />
         ))}
-
+        <circle
+          key={'inner-circle'}
+          cx={0}
+          cy={0}
+          r={innerCircleRadius - 30}
+          fill={'black'}
+          stroke={'white'}
+          strokeWidth={1}
+        />
+        {[1, 2, 3].map((idx: number) => (
+          <line
+            transform={`rotate(${120 * idx - 37})`}
+            x1={0}
+            y1={0}
+            y2={height / 2}
+            stroke={'white'}
+            strokeWidth={3}
+          />
+        ))}
+        // labels for segments
+        {['preferred', 'skilled', 'scaling'].map(
+          (segmentName: string, idx: number) => (
+            <g transform={`rotate(${20 + (idx + 1) * (360 / 3)})`}>
+              <text
+                textAnchor='middle'
+                y={innerCircleRadius / 2 - 10}
+                fontSize={15}
+                transform={`rotate(180, ${xScaleSegments(1)} ${
+                  innerCircleRadius / 2 - 10
+                })`} // rotate(a,x,y) rotate around a given point
+                fill={'white'}
+              >
+                {segmentName}
+              </text>
+            </g>
+          )
+        )}
         {data.map((d: any, techIdx: number) => {
           return (
             <g>
+              // labels for each line
+              {[1, 2, 3].map((segment: number) => (
+                <g
+                  transform={`rotate(${
+                    -30 + techIdx * (360 / 24) + 120 * segment
+                  })`}
+                >
+                  // rotate around the chart
+                  <text
+                    textAnchor='middle'
+                    y={height / 2}
+                    fontSize={15}
+                    fill={color(techIdx + (segment - 1) * 8)}
+                    transform={`rotate(180, ${xScale(techIdx)} ${
+                      height / 2 + 15
+                    })`} // rotate(a,x,y) rotate around a given point
+                  >
+                    {d.name}
+                  </text>
+                </g>
+              ))}
               {d.preferred.map((dataPoint: any, idx: number) => {
                 idx += 1;
                 let r = innerCircleRadius + idx * Math.pow(0.7, 2) * 120; //adding decay for radius size increase
