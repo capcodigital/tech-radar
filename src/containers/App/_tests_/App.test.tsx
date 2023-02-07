059f5b5c6e82aae248f356a10fb8d9efa7eae10e
kzwt-capco <klaudia.zawrotna@capco.com>
2023-02-07 18:44:27 +0000

    Updating failing unit test

--- src/containers/App/_tests_/App.test.tsx
+++ src/containers/App/_tests_/App.test.tsx
@@ -3,10 +3,6 @@ import App from "../App";
 import { BrowserRouter as Router } from "react-router-dom";
 
 it("should render App component and check for capco logo", () => {
-  render(
-    <Router>
-      <App />
-    </Router>
-  );
+  render(<App />);
   expect(screen.getByAltText("Large Logo")).toBeInTheDocument();
 });
