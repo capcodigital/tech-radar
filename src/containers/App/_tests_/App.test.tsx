279e9bb6cf997d8ac2d2297bbd0877473f3ea0a9
kzwt-capco <klaudia.zawrotna@capco.com>
2023-02-08 13:25:03 +0000

    GT actions build fail fixes

--- src/containers/App/_tests_/App.test.tsx
+++ src/containers/App/_tests_/App.test.tsx
@@ -1,6 +1,5 @@
 import { render, screen } from "@testing-library/react";
 import App from "../App";
-import { BrowserRouter as Router } from "react-router-dom";
 
 it("should render App component and check for capco logo", () => {
   render(<App />);
