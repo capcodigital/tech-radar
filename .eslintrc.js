aaca69c89e22a8cd0235972dbcb22ac8d066e691
Andreas Velounias <andreas.velounias@capco.com>
2023-03-03 17:08:41 +0000

    Change base url

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,6 @@ module.exports = {
     "@typescript-eslint/ban-ts-comment": "off",
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-non-null-assertion": "off",
-    "@typescript-eslint/no-var-requires": "off"
+    "@typescript-eslint/no-var-requires": "off",
   },
 };
--- cypress.config.ts
+++ cypress.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig } from "cypress";
-const configImport = require("./cypress/plugins/index.ts");
 export default defineConfig({
   viewportWidth: 1100,
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return configImport(on, config);
+      return require("./cypress/plugins/index.ts")(on, config);
     },
-    baseUrl: "https://master.d3qm5n59wrk3b.amplifyapp.com/",
+    baseUrl: "https://radar.capco.io/",
   },
 });
